58251b9ca1d0a93cd5b5a094326d80c8dee6403b
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-08-21 16:36:42 +0200

    fixed URL

--- src/utils/OMDbService/index.ts
+++ src/utils/OMDbService/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const OMDbService = axios.create({
   method: 'GET',
-  baseURL: 'http://www.omdbapi.com/',
+  baseURL: 'https://www.omdbapi.com/',
   params: {
     apikey: process.env.REACT_APP_OMDB_API_KEY,
   },
